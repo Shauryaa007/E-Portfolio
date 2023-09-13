92f1d5a3aa6ad13eaf1cb374cc7a4ef0adf22825
SHAURYA AWASTHI <89693063+Shauryaa007@users.noreply.github.com>
2023-09-13 17:10:48 +0530

    logo update

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,17 +6,16 @@ import text from "./images/projects/text.png";
 import pixel from "./images/projects/pixel.png";
 import major from "./images/projects/major.png";
 import Sharp from "./images/projects/Sharp.png";
-import testimonialOne from "./images/testmonial/testimonialOne.png";
-import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+//import testimonialOne from "./images/testmonial/testimonialOne.png";
+//import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.png";
 
 export {
-  logo,
+  
   bannerImg,
  text,pixel,blood,major,Sharp,eportfolio,
-  testimonialOne,
-  testimonialTwo,
+logo,
   quote,
   contactImg,
 };
